0c7d6c28bfe35dfe853e37bd3572b4699590d7b2
PengShengjieDev <speng@fintechautomation.com>
2023-07-29 14:06:16 +0800

    build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
             items: [
               {
                 label: "Tutorial",
-                to: "/docs/intro",
+                to: "/docs/ecma262/介绍",
               },
             ],
           },
