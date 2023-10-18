131f0a8e1f766db7ef4a5874b961a2550cc242d3
PengShengjieDev <speng@fintechautomation.com>
2023-10-19 01:53:52 +0800

    s

---
+++ docs/ops/nginx/nginx常用命令.md
@@ -0,0 +1,8 @@
+# nginx 常用命令
+
+|命令|说明|
+|nginx|启动nginx|
+|nginx -t|测试配置文件|
+|nginx -v|显示 nginx 的版本|
+|nginx -c path|为ngix指定一个配置文件|
+|./nginx -s reload|重启|
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -146,6 +146,27 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        apiKey: 'cb929fb7c04c08f0a6f82641efda514b',
+        indexName: 'codingj_top',
+        appId: '734SLMDMGB',
+              // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'https://www.codingj.top/',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+      // Optional: Algolia search parameters
+      searchParameters: {},
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+      //... other Algolia params
+      }
     }),
 };
 
