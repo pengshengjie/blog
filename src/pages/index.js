a061f00df6c7db76e18e1ebc709e5126187ebb6c
PengShengjieDev <speng@fintechautomation.com>
2023-12-28 01:37:07 +0800

    git add .

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 // import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
+import "./baoyou"
 import styles from "./index.module.css";
 import jpg from "./bg.webp";
 
