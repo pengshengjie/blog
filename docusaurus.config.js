39353678bf633a2abade3d3e71b47bf7e1c103d3
Shengjie Peng <speng@fintechautomation.com>
2025-01-01 11:31:37 +0800

    fix: current

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://pengshengjie.github.io",
+  url: "https://codingj.top",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
